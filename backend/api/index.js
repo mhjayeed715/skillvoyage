45c0f70473b468d53e24693f9256d750830ad602
S. M. Mehrab Hossain Jayeed <mehrabjayeed715@gmail.com>
2025-04-12 02:27:59 +0600

    Simplified CORS configuration

--- backend/api/index.js
+++ backend/api/index.js
@@ -11,9 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: '*', 
 }));
 
 // MongoDB Connection
